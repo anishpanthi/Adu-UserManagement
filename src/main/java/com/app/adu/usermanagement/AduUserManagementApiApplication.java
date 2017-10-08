29bc996b9482b421229b3e0afa2f2035e031c2bc
anishpanthi <anish.panthi@gmail.com>
2017-10-08 14:55:23 -0500

    Initial Commit

---
+++ src/main/java/com/app/adu/usermanagement/AduUserManagementApiApplication.java
@@ -0,0 +1,12 @@
+package com.app.adu.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AduUserManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AduUserManagementApiApplication.class, args);
+	}
+}
